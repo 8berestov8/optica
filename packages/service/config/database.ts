42ef719984fc9c9313e2862eef4dd0e188162f2b
Anton Berestov <8berestov8@gmail.com>
2023-08-04 20:53:02 +0200

    change: name variables

--- packages/service/config/database.ts
+++ packages/service/config/database.ts
@@ -1,22 +1,21 @@
-export default ({env}) => {
-
-  const obj = ({
+export default ({ env }) => {
+  const obj = {
     defaultConnection: "default",
     connection: {
-      client: env('DATABASE_CLIENT', 'sqlite'),
+      client: env("DATABASE_CLIENT", "sqlite"),
       connection: {
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('MYSQL_DATABASE_PORT', 3306),
-        database: env('MYSQL_DATABASE', 'optika'),
-        user: env('MYSQL_DATABASE_USER', 'root'),
-        password: env('MYSQL_DATABASE_PASSWORD', 'Pa$$w0rd'),
+        host: env("DATABASE_HOST", "localhost"),
+        port: env.int("DATABASE_PORT", 3306),
+        database: env("DATABASE_NAME", "optika"),
+        user: env("DATABASE_NAME_USER", "root"),
+        password: env("DATABASE_NAME_PASSWORD", "Pa$$w0rd"),
         ssl: {
-          rejectUnauthorized: env.bool('DATABASE_SSL', false), // For self-signed certificates
+          rejectUnauthorized: env.bool("DATABASE_SSL", false), // For self-signed certificates
         },
-      }
+      },
     },
     debug: false,
-  })
-  console.log("ENV", obj)
-  return obj
+  };
+  console.log("ENV", obj);
+  return obj;
 };
--- packages/test/config/database.js
+++ packages/test/config/database.js
@@ -1,17 +1,17 @@
-module.exports = ({env}) => ({
+module.exports = ({ env }) => ({
   defaultConnection: "default",
   connection: {
-    client: env('DATABASE_CLIENT', 'sqlite'),
+    client: env("DATABASE_CLIENT", "sqlite"),
     connection: {
-      host: env('MYSQL_DATABASE_HOST', '127.0.0.1'),
-      port: env.int('MYSQL_DATABASE_PORT', 3306),
-      database: env('MYSQL_DATABASE', 'strapi'),
-      user: env('MYSQL_USER', 'strapi'),
-      password: env('MYSQL_PASSWORD', 'strapi'),
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 3306),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USER", "strapi"),
+      password: env("DATABASE_PASSWORD", "strapi"),
       ssl: {
-        rejectUnauthorized: env.bool('DATABASE_SSL', false), // For self-signed certificates
+        rejectUnauthorized: env.bool("DATABASE_SSL", false), // For self-signed certificates
       },
-    }
+    },
   },
   debug: false,
 });
